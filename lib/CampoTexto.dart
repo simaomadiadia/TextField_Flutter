f9090aa7275156043d8c707a52188534e8e4974a
unknown <simaomadiadia@gmail.com>
2026-01-10 18:49:29 +0000

    Criar TextInput do tipo text, Criar botão  e adicionar evento para capturar valor digitado no textInput

--- lib/CampoTexto.dart
+++ lib/CampoTexto.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CampoTexto extends StatefulWidget {
   const CampoTexto({super.key});
@@ -9,8 +10,50 @@ class CampoTexto extends StatefulWidget {
 }
 
 class _CampoTextoState extends State<CampoTexto> {
+
+  TextEditingController _txtNome = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Campo de texto"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 30,left: 8,right: 8,),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Informa seu nome"
+                  ),
+                  maxLength: 20,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.green,
+                  ),
+                  onChanged: ( String valor){
+                    print("Evento onChange "+valor);
+                  },
+
+                  onSubmitted: (String valor){
+                  print("Evento onSubmitted "+valor);
+                 },
+                  controller: _txtNome,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                    print(" Voce digitou ${_txtNome.text}");
+                  },
+                  child: Text("Salvar"))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
