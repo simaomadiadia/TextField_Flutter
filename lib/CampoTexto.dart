7422a5ed1332207e37ef855b3e980e05e7d6bebd
unknown <simaomadiadia@gmail.com>
2026-01-10 18:01:23 +0000

    Alterar para estrutura basica e Criar Classe CampoTexto

---
+++ lib/CampoTexto.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class CampoTexto extends StatefulWidget {
+  const CampoTexto({super.key});
+
+  @override
+  State<CampoTexto> createState() => _CampoTextoState();
+}
+
+class _CampoTextoState extends State<CampoTexto> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
